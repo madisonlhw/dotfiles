7aa81e61f8dee9494be259c0b981b9c2839358f9
Madison Wass <madison@ccerailway.com>
2026-07-29 07:16:14 -0400

    Added C dev environment

--- flake.nix
+++ flake.nix
@@ -1,31 +1,59 @@
 {
-   description = "madison wass' configs for NixOS";
+  description = "madison wass' configs for NixOS";
    
-   inputs = {
-     nixpkgs.url = "github:nixos/nixpkgs/nixos-26.05";
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-26.05";
      
-     home-manager = {
-        url = "github:nix-community/home-manager/release-26.05";
-        inputs.nixpkgs.follows = "nixpkgs";
-     };
-   };
+    home-manager = {
+      url = "github:nix-community/home-manager/release-26.05";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
 
-   outputs = { self, nixpkgs, home-manager, ...}@inputs: {
+  outputs = { self, nixpkgs, home-manager, ...}@inputs:
+    let
+      supportedSystems = [ "x86_64-linux" "aarch64-linux" ];
+      forEachSystem = f: nixpkgs.lib.genAttrs supportedSystems (system: f nixpkgs.legacyPackages.${system});
+    in
+    {
       nixosConfigurations.akatsuki = nixpkgs.lib.nixosSystem {
-         system = "x86_64-linux";
-         modules = [
-            ./common/global-packages.nix
-            ./common/jpinput.nix
-            ./common/yubikey.nix
-            ./systems/akatsuki/hardware-configuration.nix
-            ./systems/akatsuki/configuration.nix
-            
-            home-manager.nixosModules.home-manager {
-               home-manager.useGlobalPkgs = true;
-               home-manager.useUserPackages = true;
-               home-manager.users.madison = import ./home/madison.nix;
-            }
-         ];
+        system = "x86_64-linux";
+        modules = [
+          ./common/global-packages.nix
+          ./common/jpinput.nix
+          ./common/yubikey.nix
+          ./systems/akatsuki/hardware-configuration.nix
+          ./systems/akatsuki/configuration.nix
+
+          home-manager.nixosModules.home-manager {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.users.madison = import ./home/madison.nix;
+          }
+        ];
       };
-   };
+
+      devShells = forEachSystem (pkgs: {
+        default = pkgs.mkShell {
+          packages = with pkgs; [
+            gcc
+            gnumake
+            cmake
+            pkg-config
+            gdb
+            valgrind
+            clang-tools
+          ];
+
+          shellHook = ''
+            OLD_PS1="$PS1"
+            export PS1="\[\033[1;35m\][C-Dev]\[\033[0m\] \u@\h:\w\$ "
+
+            echo "<<< C Development Environment >>>"
+            echo "Architecture Target: ${pkgs.system}"
+            echo "Compiler: $(cc --version | head -n 1)"
+          '';
+        };
+      });
+    };
 }
--- home/madison.nix
+++ home/madison.nix
@@ -1,84 +1,80 @@
 { pkgs, ...}: {
-   home.username = "madison";
-   home.homeDirectory = "/home/madison";
-   home.stateVersion = "26.05";
+  home.username = "madison";
+  home.homeDirectory = "/home/madison";
+  home.stateVersion = "26.05";
 
-   home.sessionVariables = {
-      EDITOR = "vim";
-   };
+  home.packages = with pkgs; [
+    hyfetch
+    gh
+    alacritty
+    discord-ptb
+  ];
 
-   home.packages = with pkgs; [
-      hyfetch
-      gh
-      alacritty
-      discord-ptb
-   ];
-
-   programs.git = {
-      enable = true;
-      settings = {
-         user = {
-            name = "Madison Wass";
-            email = "madison@ccerailway.com";
-         };
-         init.defaultBranch = "main";
-         core.editor = "vim";
-         safe.directory = "/home/madison/.dotfiles";
+  programs.git = {
+    enable = true;
+    settings = {
+      user = {
+        name = "Madison Wass";
+        email = "madison@ccerailway.com";
       };
-   };
+      init.defaultBranch = "main";
+      core.editor = "vim";
+      safe.directory = "/home/madison/.dotfiles";
+    };
+  };
 
-   programs.alacritty = {
-      enable = true;
-      settings.font = {
-         normal = {
-            family = "Overpass Mono";
-            style = "Regular";
-         };
-         bold = {
-            family = "Overpass Mono";
-            style = "Bold";
-         };
-         italic = {
-            family = "Overpass Mono";
-            style = "Italic";
-         };
+  programs.alacritty = {
+    enable = true;
+    settings.font = {
+      normal = {
+        family = "Overpass Mono";
+        style = "Regular";
       };
+      bold = {
+        family = "Overpass Mono";
+        style = "Bold";
+      };
+      italic = {
+        family = "Overpass Mono";
+        style = "Italic";
+      };
+    };
 
-      settings.colors = {
-         primary = {
-            background = "0x1c1b2e";
-            foreground = "0xe8e6f2";
-         };
-         cursor = {
-            text       = "0x1c1b2e";
-            cursor     = "0xf6a35c";
-         };
-         normal = {
-            black      = "0x2a2740";
-            red        = "0xeb6f92";
-            green      = "0x9dc08b";
-            yellow     = "0xf6c177";
-            blue       = "0xc4a7e7";
-            magenta    = "0xcf9bc4";
-            cyan       = "0x8ec7d0";
-            white      = "0xb9b6cf";
-         };
-      };   
-   };
+    settings.colors = {
+      primary = {
+        background = "0x1c1b2e";
+        foreground = "0xe8e6f2";
+      };
+      cursor = {
+        text       = "0x1c1b2e";
+        cursor     = "0xf6a35c";
+      };
+      normal = {
+        black      = "0x2a2740";
+        red        = "0xeb6f92";
+        green      = "0x9dc08b";
+        yellow     = "0xf6c177";
+        blue       = "0xc4a7e7";
+        magenta    = "0xcf9bc4";
+        cyan       = "0x8ec7d0";
+        white      = "0xb9b6cf";
+      };
+    };   
+  };
 
-   programs.vim = {
-      enable = true;
-      defaultEditor = true;
-      plugins = with pkgs.vimPlugins; [
-         vim-airline
-      ];
-      extraConfig = ''
+  programs.vim = {
+    enable = true;
+    defaultEditor = true;
+    plugins = with pkgs.vimPlugins; [
+      vim-airline
+    ];
+    extraConfig = ''
          set tabstop=4
          set softtabstop=4
          set shiftwidth=4
          set expandtab
-      '';
-   };
+    '';
+  };
 
-   programs.home-manager.enable = true;
+  programs.home-manager.enable = true;
 }
